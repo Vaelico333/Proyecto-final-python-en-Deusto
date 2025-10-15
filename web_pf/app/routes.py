ad6b11b09708d47f7466d08f60e0c3710df123ad
Darío <darzoga@gmail.com>
2025-10-15 15:07:27 +0200

    Creación de plantillas y base.html

--- web_pf/app/routes.py
+++ web_pf/app/routes.py
@@ -5,4 +5,18 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username':'Darío'}
-    return render_template('index.html', title='Bienvenida', user=user)
+    posts = [
+        {
+            'author': {'username':'Shirly'},
+            'body': 'Hace un día fantástico en Valladolid'
+        },
+        {
+            'author': {'username':'Madre'},
+            'body': '¡A ver si me llamas más!'
+        },
+        {
+            'author': {'username':'Aleix'},
+            'body':'Espero tu reporte, hermano.'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
