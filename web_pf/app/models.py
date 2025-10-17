e843b8405e86632cc588dfdcef5f92ff7a3fa12d
Darío <darzoga@gmail.com>
2025-10-17 02:30:12 +0200

    Creación de mecánica de login y logout

--- web_pf/app/models.py
+++ web_pf/app/models.py
@@ -2,10 +2,11 @@ from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'usuarios'
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
 
@@ -41,3 +42,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
